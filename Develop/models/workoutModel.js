6b2ffbee1bcc11711089e7c21718deba92a84c29
BetterBusinessBrian <bman1402@gmail.com>
2021-01-23 15:50:42 -0500

    full local functionality

--- Develop/models/workoutModel.js
+++ Develop/models/workoutModel.js
@@ -37,6 +37,12 @@ distance: {
 }
 }
   ]
+}, { toJSON: { virtuals: true } });
+
+workoutSchema.virtual("totalDuration").get(function() {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration
+    }, 0);
 });
 
 
--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -1,38 +1,52 @@
 
 const router = require("express").Router();
 const workout = require("../models/workoutModel.js");
+require ('mongoose');
 
 
+router.post('/api/workout', ({ body }, res) => {
+    workout.create(body)
+    .then(dbTransaction => {
+        res.json(dbTransaction);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
 
-// router.post("/api/transaction", ({ body }, res) => {
-//   Transaction.create(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+router.get("/api/workout", (req, res) => {
+  workout.find({})
+  .then(dbTransaction => {
+    console.log(dbTransaction);
+    res.json(dbTransaction);
+})
+.catch(err=> {
+    res.status(400).json(err);
+});
+});
 
-// router.post("/api/transaction/bulk", ({ body }, res) => {
-//   Transaction.insertMany(body)
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+router.put('/api/workout/:id', ({ body, params }, res) => {
+    workout.findByIdAndUpdate(params.id,
+        {$push: {exercises: body}},
+        {new: true, runValidators: true }
+        )
+    .then(dbTransaction => {
+        res.json(dbTransaction);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    })
+});
 
-// router.get("/api/transaction", (req, res) => {
-//   Transaction.find({})
-//     .sort({ date: -1 })
-//     .then(dbTransaction => {
-//       res.json(dbTransaction);
-//     })
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
+router.get('/api/workout/range', (req, res) => {
+    workout.find({}).limit(7).sort({$natural:-1})
+    .then(dbTransaction => {
+        console.log(dbTransaction);
+        res.json(dbTransaction);
+    })
+    .catch(err=> {
+        res.status(400).json(err);
+    });
+})
 
 module.exports = router;
